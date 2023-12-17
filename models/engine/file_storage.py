bc9aad1d30f246e96c425515ef206f1523b262f5
Aaron Pius <piuschbz@gmail.com>
2023-12-17 14:38:20 +0000

    Update: all method to list class passed as argument. add: delete method to delete model objects from storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,17 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if not cls:
+            return FileStorage.__objects
+
+        cls_dict = {}
+        for k, v in FileStorage.__objects.items():
+            if cls.__name__ in k:
+                cls_dict.update({k: v})
+
+        return cls_dict
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -45,6 +53,14 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """ Deletes and Object from storage"""
+
+        if obj is not None:
+            obj_key = obj.to_dict()['__class__'] + '.' + obj.id
+            if obj_key in FileStorage.__objects:
+                del self.all()[obj_key]
