9b6563e34512ab87546929f9332f977039d996cd
Aaron Pius <piuschbz@gmail.com>
2023-12-17 14:40:01 +0000

    add: set up databases for development and test

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,5 @@
+-- Sets up database for development
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
---
+++ setup_mysql_test.sql
@@ -0,0 +1,5 @@
+-- Sets up database for development
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
