5ea32b5dcd6734a4f5bd458b379bbb089375f49a
Aaron Pius <piuschbz@gmail.com>
2024-01-12 11:31:44 +0000

    Deploy and Decompress

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+Deploys archive to remote server.
+"""
+from fabric.api import put, run, env
+import os
+
+env.hosts = ['18.234.253.75', '54.174.123.116']
+
+def do_deploy(archive_path):
+    """
+    deploy function
+
+    archive_path: path to the archive on the local machine
+    return: True if operations worked correctly
+            False if operations failed or archive_path does not exist
+    """
+
+    if not os.path.exists(archive_path):
+        return False
+
+    try:
+        # Upload the archive to the /tmp/ directory on the remote server
+        put(local_path=archive_path, remote_path='/tmp/')
+
+        # Decompresses Archive
+        new_release = archive_path.split('/')[-1].replace('.tgz', '')
+        run('mkdir -p /data/web_static/releases/{}'.format(new_release))
+        run('tar -xvf /tmp/{} -C /data/web_static/releases/{}'
+            .format(archive_path.split('/')[-1], new_release))
+
+        # Updates Symbolic link
+        run('rm /data/web_static/current')
+        run('ln -s /data/web_static/releases/{} /data/web_static/current'
+            .format(new_release))
+        return True
+    except Exception as e:
+        print(f"Error: {e}")
+        return False
