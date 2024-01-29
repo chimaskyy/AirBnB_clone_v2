db4f0d32c0d5439e4f1fe8721bb3327485e37c6a
Aaron Pius <piuschbz@gmail.com>
2024-01-29 13:49:38 +0000

    He never work

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,15 +3,18 @@
 
 from flask import Flask, render_template
 from models import storage
+from models.state import State
 
 app = Flask(__name__)
+states = storage.all(State)
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list(states):
     """ Routes /states_list page."""
 
-    return render_template('7-states_list.html', states=sorted(storage.all(State).values()))
+    return render_template('7-states_list.html',
+                           states=sorted(storage.all(State).values()))
 
 
 if __name__ == '__main__':
