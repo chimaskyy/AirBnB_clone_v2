04a5da465605a7a79671a8ef696afcd071c4422b
Aaron Pius <piuschbz@gmail.com>
2024-01-29 04:02:02 +0000

    States list

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Routes for list of states. """
+
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list(states):
+    """ Routes /states_list page."""
+
+    return render_template('7-states_list.html', states=sorted(storage.all(State).values()))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
