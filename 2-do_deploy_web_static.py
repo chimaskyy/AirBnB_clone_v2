069f4b7c6b779f933f3127162cb7ec56c54769e8
chimaskyy <chiamakahj2@gmail.com>
2024-02-02 09:36:20 +0000

    more checks

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -31,6 +31,58 @@ def do_pack():
 
 
 def do_deploy(archive_path):
+        """Deploy web files to server
+        """
+        try:
+                if not (path.exists(archive_path)):
+                        return False
+
+                # upload archive
+                put(archive_path, '/tmp/')
+
+                # create target dir
+                timestamp = archive_path[-18:-4]
+                run('sudo mkdir -p /data/web_static/\
+releases/web_static_{}/'.format(timestamp))
+
+                # uncompress archive and delete .tgz
+                run('sudo tar -xzf /tmp/web_static_{}.tgz -C \
+/data/web_static/releases/web_static_{}/'
+                    .format(timestamp, timestamp))
+
+                # remove archive
+                run('sudo rm /tmp/web_static_{}.tgz'.format(timestamp))
+
+                # move contents into host web_static
+                run('sudo mv /data/web_static/releases/web_static_{}/web_static/* \
+/data/web_static/releases/web_static_{}/'.format(timestamp, timestamp))
+
+                # remove extraneous web_static dir
+                run('sudo rm -rf /data/web_static/releases/\
+web_static_{}/web_static'
+                    .format(timestamp))
+
+                # delete pre-existing sym link
+                run('sudo rm -rf /data/web_static/current')
+
+                # re-establish symbolic link
+                run('sudo ln -s /data/web_static/releases/\
+web_static_{}/ /data/web_static/current'.format(timestamp))
+        except:
+                return False
+
+        # return True on success
+        return True
+
+
+
+
+
+
+
+
+
+''''def do_deploy(archive_path):
     """Deploy archive to server"""
 
     if not os.path.exists(archive_path):
@@ -58,6 +110,6 @@ def do_deploy(archive_path):
         return True
     except Exception as e:
         print (f"Erro: {e}")
-        return False
+        return False'''
 
 
