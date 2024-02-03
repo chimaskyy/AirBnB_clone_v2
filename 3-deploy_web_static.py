6ff1a516ac6b47040e3c6da391245a2d1285849b
chimaskyy <chiamakahj@gmail.com>
2024-02-03 22:11:49 +0100

    distribute archive

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -8,7 +8,7 @@ from fabric.api import local, run, put, env
 from datetime import datetime
 import os
 
-env.hosts = ["54.175.8.50", "54.237.39.190"]
+env.hosts = ["54.210.173.228", "54.237.224.86"]
 env.user = "ubuntu"
 env.key = "~/.ssh/id_rsa"
 
@@ -37,23 +37,29 @@ def do_deploy(archive_path):
         return False
     try:
         # upload achieved path to tem dir of server
-        put(local_path=archive_path, remote_path="/tmp/")
+        put(local_path=archive_path, remote_path='/tmp/')
         path_ = os.path.basename(archive_path)
         path_without_ext = os.path.splitext(path_)[0]
         # path_ = archive_path.split('/')[-1]
         # path_without_ext = path_.replace('.tgz','')
-        run("sudo mkdir -p /data/web_static/releases/{}/".format(path_without_ext))
+
+        run("mkdir -p /data/web_static/releases/{}/".format(path_without_ext))
         # uncompress file
-        run("sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(path_, path_without_ext))
+        run("sudo tar -xzf /tmp/{} \
+            -C /data/web_static/releases/{}/".format(path_, path_without_ext))
         # Delete archive from web server
-        run("sudo rm -f /tmp/{}".format(path_))
-        run("sudo mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(path_without_ext, path_without_ext))
-        run("sudo rm -rf /data/web_static/releases/{}/web_static".format(path_without_ext))
+        run("sudo rm /tmp/{}".format(path_))
+        run('sudo mv /data/web_static/releases/{}/web_static/* \
+                /data/web_static/releases/{}\
+                /'.format(path_without_ext, path_without_ext))
+        run("sudo rm -rf /data/web_static/releases/{}\
+                /web_static".format(path_))
 
         # remove symbolic link
         run("sudo rm -rf /data/web_static/current")
         # create new symlink linked to the new server config
-        run("sudo ln -s /data/web_static/releases/{} /data/web_static/current".format(path_without_ext))
+        run("sudo ln -s /data/web_static/releases/{} \
+                /data/web_static/current".format(path_without_ext))
         return True
     except Exception as e:
         print(f"Erro: {e}")
@@ -62,6 +68,7 @@ def do_deploy(archive_path):
 def deploy():
     """creates and didtributes an archive to web servers"""
     pack = do_pack()
+
     if not pack:
         return False
     return do_deploy(pack)
