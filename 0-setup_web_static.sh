6f95d944f8c5c9ec5a381ddda7bdfb0ac22eb169
chimaskyy <chiamakahj@gmail.com>
2024-02-04 22:44:33 +0100

    fixed I guess

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -20,38 +20,46 @@ echo "Hello from testing" | sudo tee /data/web_static/releases/test/index.html >
 if [ -h /data/web_static/current ]; then
 	rm /data/web_static/current
 fi
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo ln -s /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 
 # back up nginx config file
 if [ ! -e /etc/nginx/sites-available/default.bak ]; then
-	cp /etc/nginx/sites-available/default /etc/nginx/sites-available/default.bak
+	sudo cp /etc/nginx/sites-available/default /etc/nginx/sites-available/default.bak
 fi
 
-# Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static
-sudo tee -a /etc/nginx/sites-available/default > /dev/null << EOF
+# Update the Nginx configuration to serve the content of /data/web_static/current
+
+
+sudo tee /etc/nginx/sites-available/default > /dev/null << EOF
 server {
-    listen 80;  # default_server;
-    listen [::]:80;  #default_server;
-    
+   listen 80 default_server;
+   listen [::]:80 default_server;
+   
+   server_name techinit.tech www.techinit.tech;   
+
     # Add custom header
-    add_header X-Served-By \$hostname;
+   add_header X-Served-By \$hostname;
+  
+   location  /  {
+                root     /var/www/html;
+                index    index.html;
+        }
 
-    server_name techinit.tech www.techinit.tech;
-    root /var/www/html;
-    index index.html index.htm;
+   location /hbnb_static {
+   	alias /data/web_static/current/;
+   }
+   error_page 404 /404.html;
+   location = /404.html  {
+          root /var/www/html;
+          internal;
+          return 404 'Ceci n\'est pas une page\n';
+	}
 
-    location /hbnb_static {
-    	alias /data/web_static/current/;
-    }
-    error_page 404 /404.html;
-
-    location /404 {
-    	root /var/www/html;
-	internal;
-    }
 }
 EOF
+
+#sudo sed -i '33i \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
 
 # check for synthax error
 sudo nginx -t
