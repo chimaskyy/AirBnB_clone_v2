1fb48fdf932556043d157a9886bd95b5977d21fb
chimaskyy <chiamakahj@gmail.com>
2024-02-17 04:06:21 +0100

    integer var

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -31,6 +31,11 @@ def hbnb_py(text='is cool'):
     return f"Python {text}"
 
 
+@app.route("/number/<int:n>", strict_slashes=False)
+def hbnb_int(n):
+    return f"{n} is a number"
+
+
 if __name__ == '__main__':
     # Start application
     app.run(host='0.0.0.0', port=5000, debug=True)
