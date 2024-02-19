317b3768483db3d713c067446dd0cf3115443275
chimaskyy <chiamakahj2@gmail.com>
2024-02-19 12:49:06 +0000

    state.city

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 def state_city_list():
     """Render state_list html"""
     states = storage.all(State).values()
-    return render_template("7-states_list.html", states=states)
+    return render_template("8-cities_by_states.html", states=states)
 
 
 @app.teardown_appcontext
